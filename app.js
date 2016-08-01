df0f85e9160bcd1828f5f06b15c9008ae0a05867
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 16:48:57 -0500

    math operators

---
+++ app.js
@@ -0,0 +1,27 @@
+/****************************************************
+*                   Math Operators                  *
+****************************************************/
+var apples = 4 + 76 + 100;
+var pears = 54 * 3;
+var banana = 10 % 3;
+var chops = 7;
+var pork = 67;
+
+pork++;
+
+chops = chops + 1;
+
+document.write(apples);
+document.write("<br>");
+document.write(pears);
+document.write("<br>");
+document.write(banana);
+document.write("<br>");
+document.write(chops);
+document.write("<br>");
+document.write(pork);
+pork--;
+document.write("<br>");
+document.write(pork);
+document.write("<br>");
+document.write(--pork);
